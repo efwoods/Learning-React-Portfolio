b39db0ea76deaef9cbdf10effa5095dfd79d191c
Evan Woods <22337004+efwoods@users.noreply.github.com>
2023-06-13 12:11:28 -0400

    commit

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -18,7 +18,7 @@ export default function About() {
             </p>
             <div className="flex justify-center">
               <a
-                href="#contact"
+                href="mailto:e.woods.business@icloud.com"
                 className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                 Email Me
               </a>
